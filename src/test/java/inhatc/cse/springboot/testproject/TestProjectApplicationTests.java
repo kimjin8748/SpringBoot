10325a279cf495119c38c6d0474be6cb3363183b
김용진 <kimjin8748@gamil.com>
2024-03-14 16:25:34 +0900

    frist commit

---
+++ src/test/java/inhatc/cse/springboot/testproject/TestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package inhatc.cse.springboot.testproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
