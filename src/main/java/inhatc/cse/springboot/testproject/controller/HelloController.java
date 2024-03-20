a04573fa4ef57cd72628640be806c3569b605e09
김용진 <kimjin8748@gamil.com>
2024-03-20 16:24:38 +0900

    controller

---
+++ src/main/java/inhatc/cse/springboot/testproject/controller/HelloController.java
@@ -0,0 +1,15 @@
+package inhatc.cse.springboot.testproject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+//    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @RequestMapping("hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
