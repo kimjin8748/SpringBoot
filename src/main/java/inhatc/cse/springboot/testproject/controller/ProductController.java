0317f1b136448d8c6712f6e4f851a0b42e59dc44
김용진 <kimjin8748@gmail.com>
2024-04-04 13:53:46 +0900

    mysql로 db 연동완료

--- src/main/java/inhatc/cse/springboot/testproject/controller/ProductController.java
+++ src/main/java/inhatc/cse/springboot/testproject/controller/ProductController.java
@@ -6,14 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/product-api")
 public class ProductController {
 
     private ProductService productService;
 
     @Autowired
-
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
